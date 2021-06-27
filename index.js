872b4ade7866515dc0328c4d4a46afa224f6206d
Vedant Mamgain <mamgainvedant@gmail.com>
2021-06-27 23:55:16 +0530

    Chnages

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const router = require("./routes/emailRoutes");
-const path = require("path");
-const cors = require("cors");
 require("dotenv").config();
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With,Content-Type,Accept"
+    );
+    next();
+});
 
 // app.use(express.urlencoded({ extended: true }));
 
